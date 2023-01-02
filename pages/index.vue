e576c9fbfc623c7239c118c220f7c91376e48c8e
Renato Filipi <re.rosa98@gmail.com>
2023-01-02 19:55:22 -0300

    vuetify config

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const test = () => {
+  alert("test");
+};
+</script>
+
+<template>
+  <div class="container"><v-btn @click="test">button</v-btn></div>
+</template>
+
+<style lang="css">
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
